5a2742077e393df00d5317451ad82a96480f777f
Aung Khant <aungkhant@gmail.com>
2025-06-01 21:14:31 +0700

    first commit

---
+++ src/main/java/com/turbopick/autowise/AutowiseApplication.java
@@ -0,0 +1,13 @@
+package com.turbopick.autowise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutowiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turbopick/autowise/AutowiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbopick.autowise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutowiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
