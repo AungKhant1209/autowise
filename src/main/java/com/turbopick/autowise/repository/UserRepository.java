2a8bb9b651c65beb5b8e4260815bc9a296b6c963
wy107 <u6511488@au.edu>
2025-08-29 13:15:29 +0700

    toe user

--- src/main/java/com/turbopick/autowise/repository/UserRepository.java
+++ src/main/java/com/turbopick/autowise/repository/UserRepository.java
@@ -1,10 +1,4 @@
-//package com.turbopick.autowise.repository;
-//
-//import com.turbopick.autowise.model.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Integer> {
-//
-//}
+package com.turbopick.autowise.repository;
+
+public interface UserRepository {
+}
